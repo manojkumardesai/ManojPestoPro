cb16f048e16993050fa1628265de0f983ab04dda
Manoj Kumar Desai <manoj.kumar.desai@in.verizon.com>
2020-01-20 18:59:05 +0530

    String and Encoding Assignment

---
+++ StringsAndEncoding/BetterStringLib/better-string-lib.js
@@ -0,0 +1,11 @@
+// TO DO: The below function reverses only ASCI supported format, need to make it independent
+
+function reverse(str) {
+    return str.split("").reverse().join("");
+}
+
+// TO DO: The below function equals only ASCI supported format, need to make it independent
+
+function equal(firstString, secondString) {
+    return firstString === secondString;
+}
---
+++ StringsAndEncoding/BetterStringLib/better-string-lib.test.js
@@ -0,0 +1,14 @@
+reverse('ab');
+// 'ba'
+
+reverse('foo 𝌆 bar');
+// rab 𝌆 oof
+
+reverse('mañana mañana');
+// anañam anañam
+
+equal('a', 'a');
+// true
+
+equal('mañana', 'mañana')
+// true
